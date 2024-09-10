344d50cc74e7592f07a94828de4f08d97db81a56
Dennis Coufal <dennis.coufal@gmail.com>
2024-09-10 11:23:22 +0200

    add artisan output to response and debug log

--- src/Http/Controllers/ScheduledTaskController.php
+++ src/Http/Controllers/ScheduledTaskController.php
@@ -12,14 +12,16 @@ class ScheduledTaskController extends BaseController
     public function run(Request $request)
     {
         $exitCode = Artisan::call('schedule:run');
+        $output = Artisan::output();
+        Log::debug($output);
 
         if ($exitCode !== 0) {
             // Log error if schedule:run was not successful
-            Log::error('Scheduled tasks execution failed with exit code: '.$exitCode);
+            Log::error('Scheduled tasks execution failed with exit code: '.$exitCode, ['output' => $output]);
 
-            return response()->json(['message' => 'Scheduled tasks execution failed.'], 500);
+            return response()->json(['message' => 'Scheduled tasks execution failed.', 'output' => $output], 500);
         }
 
-        return response()->json(['message' => 'Scheduled tasks executed successfully.']);
+        return response()->json(['message' => 'Scheduled tasks executed successfully.', 'output' => $output]);
     }
 }
